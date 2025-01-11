56b6214b24c252443f624da528b12d9f0f562403
MostafaAbobaker <35193789+MostafaAbobaker@users.noreply.github.com>
2025-01-11 03:15:54 +0200

    Add router Link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { ProductDetailsComponent } from './Components/product-details/product-details.component';
 import { ProductsComponent } from './Components/products/products.component';
+import { NotFound404Component } from './shared/Components/not-found404/not-found404.component';
 
 const routes: Routes = [
   { path: '',redirectTo:'home', pathMatch: 'full' },
   {path: 'home', component: HomeComponent},
   { path: 'Products', component:ProductsComponent},
   { path: 'Product/:id', component:ProductDetailsComponent},
-
+  { path: '**', component:NotFound404Component}
 ];
 
 @NgModule({
